dac0ff6d90e31fc01a91d6c57853b54682fde48c
Willian Menezes <willian.santos@dartdigital.com.br>
2020-05-19 02:35:53 -0300

    Definida a arquitetura do projeto, baixado pacotes nuggets para utilização e configurado swagger

--- CadastroProdutos/Startup.cs
+++ CadastroProdutos/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -10,11 +11,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace CadastroProdutos
 {
     public class Startup
     {
+
+        private const string DefaultCorsPolicy = "DefaultCorsPolicy";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,7 +32,19 @@ namespace CadastroProdutos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(opt =>
+            {
+                opt.EnableEndpointRouting = false;
+            });
+
+            services.AddMvcCore().AddApiExplorer().AddNewtonsoftJson();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
             services.AddControllers();
+
+            ConfigureSwagger(services);
+            ConfigureCors(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +54,10 @@ namespace CadastroProdutos
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
 
@@ -42,9 +65,57 @@ namespace CadastroProdutos
 
             app.UseAuthorization();
 
+            app.UseCors(DefaultCorsPolicy);
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                        name: "default",
+                        template: "/api/{culture}/{controller}/{action}/{id?}");
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "CadastroProdutos");
+            });
+        }
+
+        private void ConfigureSwagger(IServiceCollection services)
+        {
+            var swaggerXMLPath = Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, $"{PlatformServices.Default.Application.ApplicationName}.xml");
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Cadastro de produtos API",
+                    Description = "Microserviços para a aplicação de cadastro de produtos de um supermercado",
+                    Contact = new OpenApiContact() { Name = "Willian Menezes", Email = "willian_menezes_santos@hotmail.com", Url = new System.Uri("https://www.linkedin.com/in/willian-menezes-9932b1b9") }
+                });
+
+                c.IncludeXmlComments(swaggerXMLPath);
+            });
+
+            services.AddSwaggerGenNewtonsoftSupport();
+        }
+
+        private void ConfigureCors(IServiceCollection services)
+        {
+            services.AddCors(setup =>
+            {
+                setup.AddPolicy(DefaultCorsPolicy, builder =>
+                {
+                    builder.AllowAnyHeader();
+                    builder.AllowAnyMethod();
+                    builder.AllowAnyOrigin();
+                });
             });
         }
     }
